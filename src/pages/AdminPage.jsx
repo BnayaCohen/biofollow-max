24fb3410542039a272200fe309bf033f8835c206
Bnaya <bnaya98@gmail.com>
2023-01-21 20:39:49 +0200

    admin reducer full

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -22,7 +22,7 @@ function adminReducer(state, action) {
       return { ...state, users: action.users }
     case 'setCurrUser':
       return { ...state, currUser: action.currUser }
-    case 'setselectedDate':
+    case 'setSelectedDate':
       return { ...state, selectedDate: action.selectedDate }
     default:
       return state
@@ -30,56 +30,55 @@ function adminReducer(state, action) {
 }
 
 export function AdminPage() {
-
-  // const [filterName, setFilterName] = useState('')
-  // const [toggleShowModal, setToggleShowModal] = useState(false)
-  // const [users, setUsers] = useState([])
-  // const [currUser, setCurrUser] = useState(null)
-  // const [selectedDate, setselectedDate] = useState('');
+  const [state, dispatch] = useReducer(adminReducer, initialState);
 
   useEffect(() => {
     ; (async () => {
-      setUsers(await userService.query({ txt: filterName }))
+      const users = await userService.query({ txt: state.filterName })
+      dispatch({ type: 'setUsers', users })
     })()
   }, [])
 
   useEffect(() => {
     ; (async () => {
-      const usrs = await userService.query({ txt: filterName })
-      setUsers(usrs.filter((user) => selectedDate === '' || new Date(user.createdAt).toLocaleDateString() === selectedDate.toLocaleDateString()))
+      const allUsers = await userService.query({ txt: state.filterName })
+      const users = allUsers.filter((user) => state.selectedDate === '' || new Date(user.createdAt).toLocaleDateString() === state.selectedDate.toLocaleDateString())
+      dispatch({ type: 'setUsers', users })
     })()
-  }, [selectedDate])
+  }, [state.selectedDate])
 
   const handleChange = async ({ target }) => {
-    setFilterName(target.value)
-    setUsers(await userService.query({ txt: target.value }))
+    dispatch({ type: 'setFilterName', filterName: target.value })
+    const users = await userService.query({ txt: target.value })
+    dispatch({ type: 'setUsers', users })
   }
 
   const onRemoveUser = async (user) => {
     if (confirm('האם למחוק את ' + user.fullname)) {
       userService.removeUser(user._id)
-      setUsers(await userService.query({ txt: filterName }))
+      const users = await userService.query({ txt: state.filterName })
+      dispatch({ type: 'setUsers', users })
     }
   }
 
   const onShowUser = (user) => {
-    setCurrUser(user)
-    setToggleShowModal(true)
+    dispatch({ type: 'setCurrUser', currUser: user })
+    dispatch({ type: 'setToggleShowModal', toggleShowModal: true })
   }
 
   return (<>
     <section className='admin-page flex column align-center'>
       <img src={Logo} className="logo" />
 
-      <input className='filter-input input' value={filterName} onChange={handleChange} type="text" placeholder='חפש משתמש' />
+      <input className='filter-input input' value={state.filterName} onChange={handleChange} type="text" placeholder='חפש משתמש' />
       <div className='flex row align-center' style={{ gap: '6px', marginBottom: '15px' }}>
-        <button onClick={() => setselectedDate('')} className='user-btn' style={{ width: '-webkit-fill-available' }}>איפוס תאריך</button>
-        <DatePicker className='date-picker' selected={selectedDate} onChange={(date) => setselectedDate(date)} />
+        <button onClick={() => dispatch({ type: 'setSelectedDate', selectedDate: '' })} className='user-btn' style={{ width: '-webkit-fill-available' }}>איפוס תאריך</button>
+        <DatePicker className='date-picker' selected={state.selectedDate} onChange={(date) => dispatch({ type: 'setSelectedDate', selectedDate: date })} />
       </div>
 
-      {users?.length ?
+      {state.users?.length ?
         <section className='user-list'>
-          {users.map((user, i) =>
+          {state.users.map((user, i) =>
             <article key={i} className={`user-preview flex space-between align-center bg-${i % 2}`}>
               <div className='user-actions'>
                 <button className='user-btn red' onClick={() => onRemoveUser(user)}>הסר</button>
@@ -95,30 +94,30 @@ export function AdminPage() {
             </article>)}
         </section>
         : null}
-      {toggleShowModal ?
+      {state.toggleShowModal ?
         <section className='modal-wrapper'>
           <div className='modal-content user-show-container'>
-            <span className='close-btn' onClick={() => { setToggleShowModal(false) }}>X</span>
-            <h2>{currUser.fullname}</h2>
-            <div>{currUser.digits}</div>
-            <h3>{new Date(currUser.createdAt).toLocaleTimeString('he-IL', { day: "numeric", month: "numeric", year: "numeric", hour: '2-digit', minute: '2-digit' })}</h3>
-            <p>פעם: <span>{currUser.counter || '?'}</span> זמן: <span>{+currUser.times?.toFixed(1) || '?'}</span></p>
+            <span className='close-btn' onClick={() => dispatch({ type: 'setToggleShowModal', toggleShowModal: false })}>X</span>
+            <h2>{state.currUser.fullname}</h2>
+            <div>{state.currUser.digits}</div>
+            <h3>{new Date(state.currUser.createdAt).toLocaleTimeString('he-IL', { day: "numeric", month: "numeric", year: "numeric", hour: '2-digit', minute: '2-digit' })}</h3>
+            <p>פעם: <span>{state.currUser.counter || '?'}</span> זמן: <span>{+state.currUser.times?.toFixed(1) || '?'}</span></p>
 
             <section className='flex space-around' style={{ gap: '40px', marginTop: '10px' }}>
               <div>
-                {currUser.results[2].map((num, i) =>
+                {state.currUser.results[2].map((num, i) =>
                   <h3 key={i} className={`bg-${1 - i % 2}`}>
                     {num}
                   </h3>)}
               </div>
               <div>
-                {currUser.results[1].map((num, i) =>
+                {state.currUser.results[1].map((num, i) =>
                   <h3 key={i} className={`bg-${i % 2}`}>
                     {num}
                   </h3>)}
               </div>
               <div>
-                {currUser.results[0].map((num, i) =>
+                {state.currUser.results[0].map((num, i) =>
                   <h3 key={i} className={`bg-${1 - i % 2}`}>
                     {num}
                   </h3>)}
